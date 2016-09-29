cda267266c37441a03add586a5245d71118c9c0b
grace <darrellulm@gmail.com>
2016-09-29 14:38:46 -0400

    Start test for greedy algorithm.

--- knapsack.py
+++ knapsack.py
@@ -1,6 +1,22 @@
+#PySpark Test Functions for Knapsack in Progress
+
 
 # Knapsack 0-1 function wieights, values and size n.
-def knapsack(W, weights, values, n):
+from pyspark.sql import Row
+from pyspark.sql.functions import lit
+from pyspark.sql.functions import col
+
+# Greedy implementation of 0-1 Knapsack algorithm.
+def knapsack(knapsackDF, W):
+    knapsackDF.withColumn("ratio", lit(knapsackDF.values / knapsackDF.weights))
+    knapsackDF.sort(col("ratio").desc())
     k = 0
     return k
+
+
+knapsackData = [('thing1', 1, 2), ('thing2', 2, 3), ('thing3', 4, 5)]
+knapsackDF = sqlContext.createDataFrame(knapsackData, ['item', 'weights', 'values'])
+print weightsDF.count()
+k = knapsack(knapsackDF, 5)
+print k
 
