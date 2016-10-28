cf6bd5f169648ec06ce9d5410f13e0bb136ec079
grace <darrellulm@gmail.com>
2016-10-28 18:59:47 -0400

    intermediate setup to take partial sums, eventually of ratio, to order by weight

--- knapsack.py
+++ knapsack.py
@@ -1,5 +1,3 @@
-#PySpark Test Functions for Knapsack in Progress
-
 
 # Knapsack 0-1 function wieights, values and size n.
 from pyspark.sql import Row
@@ -8,15 +6,17 @@ from pyspark.sql.functions import col
 
 # Greedy implementation of 0-1 Knapsack algorithm.
 def knapsack(knapsackDF, W):
-    knapsackDF.withColumn("ratio", lit(knapsackDF.values / knapsackDF.weights))
-    knapsackDF.sort(col("ratio").desc())
-    k = 0
-    return k
+    ratioDF = knapsackDF.withColumn("ratio", lit(knapsackDF.values / knapsackDF.weights))
+    ratioDF.sort(col("ratio").desc())
+    partialSumsDF = (ratioDF
+         .map(lambda x: x)
+      )
+    return partialSumsDF
 
 
 knapsackData = [('thing1', 1, 2), ('thing2', 2, 3), ('thing3', 4, 5)]
-knapsackDF = sqlContext.createDataFrame(knapsackData, ['item', 'weights', 'values'])
-print weightsDF.count()
+knapsackData = sqlContext.createDataFrame(knapsackData, ['item', 'weights', 'values'])
 k = knapsack(knapsackDF, 5)
-print k
+print k.take(3)
+
 
