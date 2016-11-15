1365379ae67bf271fa61949efec334aa2653178b
Darrell Ulm <darrellulm@gmail.com>
2016-11-14 19:15:13 -0500

    prototype greedy spark function, working, need to test data

--- knapsack.py
+++ knapsack.py
@@ -1,5 +1,6 @@
 # Knapsack 0-1 function wieights, values and size n.
 import sys
+import random
 import pyspark.sql.functions as func
 from pyspark.sql.window import Window
 from pyspark.sql import Row
@@ -9,10 +10,12 @@ from pyspark.sql.functions import col
 
 # Greedy implementation of 0-1 Knapsack algorithm.
 def knapsack(knapsackDF, W):
+    # Add ratio of values / weights column.
     ratioDF = knapsackDF.withColumn("ratio", lit(knapsackDF.values / knapsackDF.weights)).sort(col("ratio").desc())
 
+    # Calculate the partial sums of the ratios.
     ratioDF.registerTempTable("tempTable")
-    cumRatioSumDF = sqlContext.sql("""
+    RatioPartialSumDF = sqlContext.sql("""
         SELECT
             item,
             weights,
@@ -23,18 +26,25 @@ def knapsack(knapsackDF, W):
         tempTable
         """)
 
-    # @TODO: Get the max number of items, greedy less than or equal to W in Spark.
+    # Get the max number of items, greedy less than or equal to W in Spark.
+    RatioPartialSumDF = RatioPartialSumDF.filter(col("partSum") <= W)
+    return RatioPartialSumDF
 
-    return cumRatioSumDF
 
+# Test the Knapsack function
+
+# Problem size
+N = 10
 
 # Setup sample data for knapsack.
-knapsackData = [('thing1', 1, 2), ('thing2', 2, 3), ('thing3', 4, 5)]
+# knapsackData = [('thing1', 1, 2), ('thing2', 2, 3), ('thing3', 4, 5), ('thing4', 4, 5), ('thing5', 4, 5), ('thing6', 4, 5)]
+knapsackData = [('item_' + str(k), random.randint(1, 100), random.randint(1, 100)) for k in range(N)]
+
 # Make a dataframe with item(s), weight(s), and value(s) for the knapsack.
 knapsackData = sqlContext.createDataFrame(knapsackData, ['item', 'weights', 'values'])
 
 # Call the knapsack greedy function, with data and size 5.
-k = knapsack(knapsackData, 5.0)
-print k.take(3)
+k = knapsack(knapsackData, random.randint(1, 100 * N / 2))
+print k.take(N)
 
 
