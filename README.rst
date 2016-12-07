feacbcbffcf0c45dd793912968f5ce483286dec0
Darrell Ulm <darrellulm@gmail.com>
2016-12-06 22:53:10 -0500

    Add to setup.py

---
+++ README.rst
@@ -0,0 +1,2 @@
+knapsackApprox
+==============
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+
+
+def setup_package():
+    metadata = dict(
+        name='knapsackApprox',
+        version='0.1.0',
+        description='Greedy Knapsack Algorithm on PySpark',
+        author='Darrell Ulm',
+        license='Apache License, Version 2.0',
+        url='https://github.com/drulm/Spark_Knapsack',
+        packages=find_packages(),
+        long_description=open('./README.rst').read(),
+        install_requires=open('./requirements.txt').read().split()
+    )
+
+    setup(**metadata)
+
+
+if __name__ == '__main__':
+    setup_package()
