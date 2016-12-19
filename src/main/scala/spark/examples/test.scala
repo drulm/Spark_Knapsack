0537c82d76714bfbab6c5810677fd4fca8b609f8
Darrell Ulm <darrellulm@gmail.com>
2016-12-19 14:58:32 -0500

    Start main test scala program, restructure dirs

---
+++ src/main/scala/spark/examples/test.scala
@@ -0,0 +1,13 @@
+package spark.examples
+
+import spark.lib.knapsack
+import org.apache.spark.rdd.RDD
+import org.apache.spark.functions.random
+import org.apache.spark.{SparkContext, SparkConf}
+
+object knapsackExample {
+
+  def main(args: Array[String]): Unit =	{
+    println("test")
+  }
+}
