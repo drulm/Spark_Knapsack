43d3e9119687f52f7921e354da06c6c33d5a7efe
Darrell Ulm <darrellulm@gmail.com>
2016-12-19 17:49:47 -0500

    Val fixes, deal with list of tuples

--- src/main/scala/spark/examples/test.scala
+++ src/main/scala/spark/examples/test.scala
@@ -1,8 +1,8 @@
 // -----------------------------------------------------------------
 //import spark.lib.knapsack
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkContext, SparkConf}
+//import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.mllib.random.RandomRDDs._
+import scala.collection.mutable.ListBuffer
 
 /*
   --------------------------------------------
@@ -13,13 +13,6 @@ import org.apache.spark.mllib.random.RandomRDDs._
 // Pull in the knapsack library.
 // import spark.lib.knapsack
 
-// Create the SparkContext.
-val sc = (SparkSession
-  .builder
-  .appName("Knapsack Approximation Algorithm Test")
-  .getOrCreate()
-  )
-
 // Knapsack problem size.
 val N = 10
 
@@ -27,10 +20,11 @@ val N = 10
 val r = scala.util.Random
 
 // Setup sample data for knapsack.
-val knapsackDataList = List[(String, Float, Float)]()
+val knapsackDataList = new ListBuffer[(String, Float, Float)]()
 for(k <- 1 to 10) {
-  val knapsackDataList = knapsackDataList :+ ("item_" ++ str(k), r.nextFloat(10), r.nextFloat(10))
+  val knapsackDataList += ("item_" ++ str(k), r.nextFloat(10), r.nextFloat(10))
 }
+val knapsackData = knapsackDataList.toList
 
 // Make a Dataframe with item(s), weight(s), and value(s) for the knapsack.
 val knapsackData = sc.parallelize(knapsackDataList).toDF("item", "weights", "values")
@@ -41,7 +35,9 @@ println(knapsackData.take(N))
 println("\n")
 
 // Create a random maximum weight
-val W = random.uniform(N * 0.3, N * 0.6)
+val start = N * 0.3
+val end = N * 0.6
+val W = (math.random * (end - start) + start);
 
 // Show the weight.
 println("W: ")
@@ -50,12 +46,12 @@ println("\n")
 
 // Call the knapsack greedy approximation function, with data and size 5.
 val knapTotals = Array()
-val k = knapsack.knapsackApprox(knapsackData, W, knapTotals)
+//val knapResults = knapsack.knapsackApprox(knapsackData, W, knapTotals)
 
 // Show the results Dataframe.
-println("Selected Elements:")
-println(k.take(N))
-println("\n")
+// println("Selected Elements:")
+// println(knapResults.take(N))
+// println("\n")
 
 // Show totals for selected elements of knapsack.
 println("Totals:")
