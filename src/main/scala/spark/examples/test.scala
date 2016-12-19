375a57bc6f538ac5617274b3edf2f5c60a1c8637
Darrell Ulm <darrellulm@gmail.com>
2016-12-19 15:10:04 -0500

    Fix import

--- src/main/scala/spark/examples/test.scala
+++ src/main/scala/spark/examples/test.scala
@@ -1,9 +1,9 @@
 package spark.examples
 
-import spark.lib.knapsack
+//import spark.lib.knapsack
 import org.apache.spark.rdd.RDD
-import org.apache.spark.functions.random
 import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.mllib.random.RandomRDDs._
 
 object knapsackExample {
 
