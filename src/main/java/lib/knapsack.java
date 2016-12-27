5f91cedb5d9d6584b64ec1a9f51adb4c1b7a2f36
Darrell Ulm <darrellulm@gmail.com>
2016-12-27 14:21:33 -0500

    Add function for knapsack Approx in Java

--- src/main/java/lib/knapsack.java
+++ src/main/java/lib/knapsack.java
@@ -1,13 +1,19 @@
 package com.github.drulm.Spark_Knapsack
 
+import org.apache.spark;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-public class knapsackApprox {
-    SparkConf conf = new SparkConf().setAppName("Knapsack Approx").setMaster("local");
-    JavaSparkContext sc = new JavaSparkContext(conf);
+
+public class knapsack {
+    public static DataFrame knapsackApprox(DataFrame knapsackDF, double W) {
+
+        SparkConf conf = new SparkConf().setAppName("Knapsack Approx").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+    }
 
 
 }
