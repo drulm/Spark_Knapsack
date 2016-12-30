6ca1f929dfc68e0c029fe9b60d2451027879f7a4
Darrell Ulm <darrellulm@gmail.com>
2016-12-30 14:34:09 -0500

    Some changes

--- src/main/java/lib/knapsack.java
+++ src/main/java/lib/knapsack.java
@@ -2,8 +2,14 @@ package com.github.drulm.Spark_Knapsack;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.DataFrame;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 
 
 public class knapsack {
