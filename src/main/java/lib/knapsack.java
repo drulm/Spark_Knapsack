2defe71eadd6d0fcca4253bd24da9e77eb8d127f
Darrell Ulm <darrellulm@gmail.com>
2017-01-04 18:03:34 -0500

    Convert to Java vars, place conversion code.

--- src/main/java/lib/knapsack.java
+++ src/main/java/lib/knapsack.java
@@ -11,40 +11,41 @@ import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
-/* Scala code for reference:
-
-  def knapsackApprox(knapsackDF: DataFrame, W: Double): DataFrame = {
-
-    // Add ratio of values / weights column.
-    val ratioDF = knapsackDF.withColumn("ratio", knapsackDF("values") / knapsackDF("weights"))
-    val newRatioDF = (ratioDF
-      .filter(ratioDF("weights") <= W)
-      .sort(ratioDF("ratio").desc)
-      )
-
-    // An sql method to calculate the partial sums of the ratios.
-    newRatioDF.createOrReplaceTempView("tempTable")
-
-    val partialSumWeightsDF = spark.sql("SELECT item, weights, values, ratio, sum(weights) OVER (ORDER BY ratio desc) as partSumWeights FROM tempTable")
-
-    // Get the max number of items, less than or equal to W in Spark.
-    val partialSumWeightsFilteredDF = (
-      partialSumWeightsDF
-        // partialSumWeightsDF.sort(partialSumWeightsDF("ratio").desc)
-        .filter(partialSumWeightsDF("partSumWeights") <= W)
-      )
-
-    // Return the solution elements with total values, weights and count.
-    partialSumWeightsDF
-  }
- */
 
 public class knapsack {
+
+    //  def knapsackApprox(knapsackDF: DataFrame, W: Double): DataFrame = {
     public static DataFrame knapsackApprox(DataFrame knapsackDF, double W) {
+
+        Dataframe ratioDF;
+        Dataframe newRatioDF;
+        Dayaframe partialSumWeightsDF;
+        Dataframe partialSumWeightsFilteredDF;
 
         SparkConf conf = new SparkConf().setAppName("Knapsack Approx").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        // Add ratio of values / weights column.
+
+        /*val ratioDF = knapsackDF.withColumn("ratio", knapsackDF("values") / knapsackDF("weights"))
+        val newRatioDF = (ratioDF
+                .filter(ratioDF("weights") <= W)
+                .sort(ratioDF("ratio").desc)
+        )*/
+
+        // An sql method to calculate the partial sums of the ratios.
+        // newRatioDF.createOrReplaceTempView("tempTable")
+
+        // val partialSumWeightsDF = spark.sql("SELECT item, weights, values, ratio, sum(weights) OVER (ORDER BY ratio desc) as partSumWeights FROM tempTable")
+
+        // Get the max number of items, less than or equal to W in Spark.
+        /*val partialSumWeightsFilteredDF = (
+                partialSumWeightsDF
+                        // partialSumWeightsDF.sort(partialSumWeightsDF("ratio").desc)
+                        .filter(partialSumWeightsDF("partSumWeights") <= W)
+        )*/
+
+        return partialSumWeightsFilteredDF;
     }
 
 
